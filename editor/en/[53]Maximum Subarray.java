da937f0aa3aa0146c763567fdb5b7d3f87ec2d87
swhong <swhong@zuminternet.com>
2020-06-22 16:44:15 +0900

    #53 [easy] - 완료

---
+++ editor/en/[53]Maximum Subarray.java
@@ -0,0 +1,35 @@
+//Given an integer array nums, find the contiguous subarray (containing at least
+// one number) which has the largest sum and return its sum. 
+//
+// Example: 
+//
+// 
+//Input: [-2,1,-3,4,-1,2,1,-5,4],
+//Output: 6
+//Explanation: [4,-1,2,1] has the largest sum = 6.
+// 
+//
+// Follow up: 
+//
+// If you have figured out the O(n) solution, try coding another solution using 
+//the divide and conquer approach, which is more subtle. 
+// Related Topics Array Divide and Conquer Dynamic Programming
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for(int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if(max < sum) {
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
